f8b0850ece49cb4e9cc6753fe25eadb8d82a3ffa
HHansi <h.hansi1991@gmail.com>
2023-07-18 14:02:11 +0100

    002: Adding NER-CV run script

--- experiments/ner/ner_cv_gpu.sbatch.sh
+++ experiments/ner/ner_cv_gpu.sbatch.sh
@@ -14,4 +14,5 @@
 #conda activate tf
 source activate accord
 
-srun python3 experiments/ner/ner_cv_experiments.py --model_name bert-base-cased --model_type bert --k_folds 2
+#srun python3 experiments/ner/ner_cv_experiments.py --model_name bert-base-cased --model_type bert --k_folds 2
+python -m experiments.ner.ner_cv_experiments --model_name bert-base-cased --model_type bert --k_folds 2
