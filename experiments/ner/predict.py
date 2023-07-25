87ea61d361fa7ec7fbc71cae65e4b11426c96d53
HHansi <h.hansi1991@gmail.com>
2023-07-25 10:11:12 +0100

    002: Adding NER experiments

--- experiments/ner/predict.py
+++ experiments/ner/predict.py
@@ -1,10 +1,18 @@
 # Created by Hansi at 24/07/2023
+import argparse
+
 from experiments.ner.ner_config import ner_args
-from experiments.ner.ner_cv_experiments import MODEL_TYPE
 from text_classification.ner.ner_model import NERModel
 
+parser = argparse.ArgumentParser(description='''evaluates multiple models  ''')
+parser.add_argument('--model_name', required=False, help='model name', default="bert-large-cased")
+parser.add_argument('--model_type', required=False, help='model type', default="bert")
+arguments = parser.parse_args()
 
-model = NERModel(MODEL_TYPE, ner_args['best_model_dir'], args=ner_args)
+MODEL_TYPE = arguments.model_type
+MODEL_NAME = arguments.model_name
+
+model = NERModel(MODEL_TYPE, MODEL_NAME, args=ner_args)
 
 input_text = ''
 while input_text != 'exit':
@@ -12,3 +20,11 @@ while input_text != 'exit':
     predictions, raw_outputs = model.predict([input_text])
     print(predictions)
 
+    final_predictions = []
+    for prediction in predictions:
+        raw_prediction = []
+        for word_prediction in prediction:
+            for key, value in word_prediction.items():
+                raw_prediction.append(value)
+        final_predictions.append(raw_prediction)
+    print(' '.join(final_predictions[0]))
