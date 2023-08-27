fad7ce396963053a9eeffefbc6f4fab1ca39f912
HHansi <h.hansi1991@gmail.com>
2023-08-27 21:00:39 +0100

    003: Adding RE-CV run script

---
+++ experiments/relation_extraction/re_cv_gpu.sbatch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=re
+#SBATCH --output=output.txt
+#SBATCH --error=error.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --time=10:00:00
+#SBATCH --mem-per-cpu=100
+
+
+#module purge
+#module load anaconda3
+#conda activate tf
+source activate accord
+
+python -m experiments.relation_extraction.re_cv_experiment --model_name bert-base-cased --model_type bert --k_folds 5 --wandb_api_key API_KEY
