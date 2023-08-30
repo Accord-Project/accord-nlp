cbd3144d52559fceae35911ce9e156f26c2819aa
HHansi <h.hansi1991@gmail.com>
2023-08-30 14:28:07 +0100

    006: Updating setup files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     distname="accord-nlp",
-    version="0.1.2",
+    version="0.1.3",
     author="Hansi Hettiarachchi",
     author_email="hansi.h.hettiarachchi@gmail.com",
     description="ACCORD - Information extraction from text",
