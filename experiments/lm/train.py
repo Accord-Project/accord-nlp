33006525a7e54985788ebdf5d2d8616572d75111
HHansi <h.hansi1991@gmail.com>
2023-12-13 19:26:48 +0000

    007: Adding LM

--- experiments/lm/train.py
+++ experiments/lm/train.py
@@ -27,24 +27,27 @@ wandb_kwargs = {'name': f"{MODEL_NAME.split('/')[-1]}"}
 
 
 with open('output_file.txt', 'wb') as wfd:
-    # for f in ['../../data/lm/sentences.csv']:
-    for f in ['data/lm/sentences.csv']:
+    for f in ['../../data/lm/sentences.csv']:
+    # for f in ['data/lm/sentences.csv']:
         with open(f,'rb') as fd:
             next(fd)
             shutil.copyfileobj(fd, wfd)
 
-with open('output_file.txt') as f:
+with open('output_file.txt', encoding='utf-8') as f:
     lines = f.read().splitlines()
 
 train_lines = lines[:int(len(lines)*.8)]
 test_lines = lines[int(len(lines)*.8):len(lines)]
 
-with open('train.txt', 'w') as f:
+print(f'train size: {len(train_lines)}')
+print(f'test size: {len(test_lines)}')
+
+with open('train.txt', 'w', encoding='utf-8') as f:
     # write each integer to the file on a new line
     for line in train_lines:
         f.write(str(line) + '\n')
 
-with open('test.txt', 'w') as f:
+with open('test.txt', 'w', encoding='utf-8') as f:
     # write each integer to the file on a new line
     for line in test_lines:
         f.write(str(line) + '\n')
@@ -54,12 +57,12 @@ model_args = LanguageModelingArgs()
 model_args.reprocess_input_data = True
 model_args.overwrite_output_dir = True
 model_args.num_train_epochs = 25
-model_args.dataset_type = "simple"
+model_args.dataset_type = "line_by_line"
 model_args.train_batch_size = 16
 model_args.eval_batch_size = 32
 model_args.learning_rate = 3e-5
 model_args.evaluate_during_training = True
-model_args.evaluate_during_training_steps = 30000
+model_args.evaluate_during_training_steps = 50
 model_args.save_eval_checkpoints = True
 model_args.save_best_model = True
 model_args.save_recent_only = True
@@ -81,3 +84,4 @@ model.train_model(train_file, eval_file=test_file)
 
 # Evaluate the model
 result = model.eval_model(test_file)
+print(result)
