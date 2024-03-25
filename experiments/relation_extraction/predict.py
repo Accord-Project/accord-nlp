92dca1cebf884e99507376a46022f0b0336740bc
HHansi <h.hansi1991@gmail.com>
2024-03-25 18:09:46 +0000

    009: Refactoring-RE model

--- experiments/relation_extraction/predict.py
+++ experiments/relation_extraction/predict.py
@@ -1,7 +1,11 @@
 # Created by Hansi on 25/03/2024
 from accord_nlp.text_classification.relation_extraction.re_model import REModel
 
-model = REModel('roberta', 'ACCORD-NLP/re-roberta-large')
+re_args = {
+    "special_tags": ["<e1>", "<e2>"],  # Should be either begin_tag or end_tag
+}
+
+model = REModel('roberta', 'ACCORD-NLP/re-roberta-large', args=re_args)
 predictions, raw_outputs = model.predict(['The <e1>gradient<\e1> of the passageway should not exceed <e2>five per cent</e2>.'])
 print(predictions)
 
