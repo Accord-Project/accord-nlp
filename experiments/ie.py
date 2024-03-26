a3cd79cf432d7644e3ff75cad85a0fc6587b532a
HHansi <h.hansi1991@gmail.com>
2024-03-26 17:55:00 +0000

    009: Refactoring-IE pipeline

--- experiments/ie.py
+++ experiments/ie.py
@@ -5,11 +5,11 @@ if __name__ == '__main__':
     # sentence = 'Perimeter insulation should be continuous and have a minimum thickness of 25mm.'
     # sentence = 'The access route for pedestrians/wheelchair users shall not be steeper than 1:20.'
     sentence = 'The gradient of the passageway should not exceed five per cent.'
-    ie = InformationExtractor()
-    ie.sentence_to_graph(sentence)
+    # ie = InformationExtractor()
+    # ie.sentence_to_graph(sentence)
 
-    ner_model_info = ('roberta', 'ACCORD-NLP/ner-roberta-large'),
-    re_model_info = ('roberta', 'ACCORD-NLP/re-roberta-large'),
+    ner_model_info = ('roberta', 'ACCORD-NLP/ner-roberta-large')
+    re_model_info = ('roberta', 'ACCORD-NLP/re-roberta-large')
     ie = InformationExtractor(ner_model_info=ner_model_info, re_model_info=re_model_info)
     ie.sentence_to_graph(sentence)
 
