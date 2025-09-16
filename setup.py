f1bc23101b5eec36926732728f05ba1e3001de15
Hansi Hettiarachchi <h.hansi1991@gmail.com>
2025-09-16 13:52:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     distname="accord-nlp",
-    version="1.0.1",
+    version="1.0.2",
     author="Hansi Hettiarachchi",
     author_email="hansi.h.hettiarachchi@gmail.com",
     description="ACCORD-NLP: Transformer/language model-based information extraction from regulatory text",
